0351d183a160ee05726ad0f206de8206ff656137
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2019-01-03 10:30:30 -0300

    feat: first commit! Learning dart in a awersome way :rocket:

---
+++ src/main.dart
@@ -0,0 +1,6 @@
+import './utils/surpriseBox.dart' as surpriseBox;
+
+main() {
+  print('Maybe we just sum some numbers');
+  print("${surpriseBox.sum(10, 20)}");
+}
---
+++ src/utils/surpriseBox.dart
@@ -0,0 +1,5 @@
+library surpriseBox;
+
+num sum(num number1, num number2) {
+  return number1 + number2;
+}
